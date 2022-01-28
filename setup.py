cc37b05343fedc22a5dcc0a9149dc94aac921002
rknaebel <rknaebel@uni-potsdam.de>
2022-01-28 15:41:17 +0100

    update setup, requirements, license

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="stagedp",
+    version="1.0.0",
+    description="Discourse parsing.",
+    author="René Knaebel",
+    author_email="rene.knaebel@uni-potsdam.de",
+    license="MIT",
+    url="https://github.com/rknaebel/stagedp",
+    packages=find_packages(),
+    keywords="nlp discourse analysis rst parser")
